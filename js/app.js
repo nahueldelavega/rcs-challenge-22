3239131bc8096aecebefc5199db00b9e18533caf
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-05-15 17:32:16 -0300

    i think is ready

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-const publishFwitt = () =>{
-    let fwittPublish = document.querySelector('#fwitt').value;
-    let fwittPublished = document.querySelector('#fwitt-published');
+const publish = (event) =>{
+    event.preventDefault();
+    let commentFwitt = document.querySelector('#comment-text').value;
+    let fwittSection = document.querySelector('#comment-section');
     let newFwitt = document.createElement('div');
-    newFwitt.classList.add('');
+    newFwitt.classList.add('text-light');
+    let id = Math.floor(Math.random()*100);
+    newFwitt.id= id;
+    newFwitt.innerHTML = `${commentFwitt}`;
+    fwittSection.appendChild(newFwitt)
+    document.getElementById('comment-form').reset()
 }
