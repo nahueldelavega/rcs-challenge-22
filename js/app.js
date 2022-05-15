f86b04a59ec164d45e1f4a8864ac94b566b5ef1a
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-05-14 22:23:14 -0300

    in process challenge 22

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const publishFwitt = () =>{
+    let fwittPublish = document.querySelector('#fwitt').value;
+    let fwittPublished = document.querySelector('#fwitt-published');
+    let newFwitt = document.createElement('div');
+    newFwitt.classList.add('');
+}
